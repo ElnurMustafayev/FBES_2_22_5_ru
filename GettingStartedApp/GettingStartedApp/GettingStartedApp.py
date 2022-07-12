5c8a9d524b63b04865a161130731ab78c6f63952
Elnur Mustafayev <e.mustafayev@itstep.edu.az>
2022-07-12 21:25:00 +0400

    Add variables

--- GettingStartedApp/GettingStartedApp/GettingStartedApp.py
+++ GettingStartedApp/GettingStartedApp/GettingStartedApp.py
@@ -1,8 +1,24 @@
-this is integer
-number = 123
+## this is integer
+## London is the capital of GB
+#number = 123
 
-this is string
-text = "Hello World!"
+## this is string
+#text = "Hello World!"
 
-this is float
-money = 12.5
+## this is float
+#money = 12.5
+
+
+
+#name = "Elnur"
+#surname = "Mustafayev"
+
+#print(number)
+
+
+#num = 123
+
+
+#variable = 18
+#variable = "qwerty"
+#print(type(variable))
