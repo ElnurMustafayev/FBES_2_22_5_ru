b8aa44c4980cb529f4cb910df31695b8215e34b0
Elnur Mustafayev <e.mustafayev@itstep.edu.az>
2022-07-14 22:00:05 +0400

    Add Operation and Convertation

---
+++ OperationsApp/OperationsApp/OperationsApp.py
@@ -0,0 +1,115 @@
+#name = input("Input your name: ")
+
+#print("Your name is: " + name)
+
+
+#print("Hello World" + "!")
+#num = 500
+#num = 124 / 2
+
+#print(type(num))
+
+#print(123 + 1)
+#print(num + 1)
+#print(num - 1)
+#print(100 * 2)
+#print(100 / 2)
+
+#print(type(100 / 2))
+
+
+
+#num = (2 + 2) * 2 + 3       # 11
+#print(num)
+
+
+#print(type((1.0 * 4)))
+
+
+
+
+#num1 = 100
+#num2 = 50.2
+
+#print(num1 + num2)
+
+
+
+
+# print(5 % 3)      # 2
+
+#num = 100
+#num = 23 % 3
+#print(num + 5)     # 7
+
+#num = 100
+#power = 6
+#print(num ** power)
+
+#print(2 ** 4)       # 16
+
+#print(100 / 3)      # 33.333...
+#print(100 // 3)     # 33
+
+
+
+
+
+
+
+#num = 100
+
+#print(num + 1)     # 101
+#print(num)         # 100
+
+
+#num = num + 5
+#num = num + 5
+#num = num + 5
+#num = num + 5
+#num = num + 5
+#print(num)
+
+
+
+#num = 100
+
+#num = num + 1
+#num += 1
+
+#print(num)
+
+
+#num *= 2
+#num / 10
+#num += 15
+#print(num)
+
+
+
+#num = 5
+
+#num **= 2
+#num %= 6
+#num += num
+
+#print(num)
+
+
+
+#num = 5
+#num *= num * 2
+#num /= 10 // 3
+#print(num)
+
+
+
+#age = input()
+#varType = type(age)
+#print(varType)
+
+#age = input("input your age: ")
+
+#print(int(age) + 10)
+
+#print(type(age))            # str
