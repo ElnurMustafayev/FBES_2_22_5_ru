00e60630de1dcc40c436b63ff2f1feda2acd709e
Elnur Mustafayev <e.mustafayev@itstep.edu.az>
2022-07-19 21:51:54 +0400

    Add condition operations

---
+++ Python/ConditionsApp/ConditionsApp/ConditionsApp.py
@@ -0,0 +1,119 @@
+#number = 9
+
+# int float str bool
+#print(type(number > 0))
+#print(number < 0)
+
+#print(10 > 10)
+#print(5 > 2 + 6)
+
+#number = 8
+#print(10 + 3 > 20 - number)
+
+#print(5 >= 5)
+#print(10 <= 20)
+#print(5 <= 10 - 5)
+
+#print(123 == 123)
+#print(123 != 123)
+
+#num = 10
+#print(num == 12)
+
+
+#num = 10 / 2
+#print(num == 5)
+
+
+#result = 10 > 123
+#print(result)
+
+
+#print("test " == " test")
+#print(10.5 > 10.5 + 1)
+
+
+
+#text = input()
+#num = 123
+#floatNum = 12.5
+#condition = False
+
+
+#check = True
+
+#print(check != True)
+#print(check != (10 > 5))
+#print(((15 == 10) != check) != (check == check))
+
+
+
+#age = int(input("Input your age: "))
+#isAdult = age >= 18
+
+#print(isAdult)
+
+
+#num1 = float(input("Input the first number: "))
+#num2 = float(input("Input the second number: "))
+
+#isEqual = num1 == num2
+
+#print(isEqual)
+
+
+
+#check = 456
+##message = f"Result is: {check}"
+#print("Result is: *", check, "*")
+#print(f"Result is: *{check}*")
+
+
+
+# money = 1000 (by default)
+
+# "How much to add?" (example: 100)
+
+# "Your money: 1100$"
+
+
+
+
+#money = 1000.
+#moneyToAdd = float(input("How much to add? "))
+#money += moneyToAdd
+
+#print(f"Your money: {money}$")
+
+#print(money)
+
+
+
+# 3 numbers
+
+# count of number which are better than 0
+
+#num1 = float(input("input 1 number: "))
+#num2 = float(input("input 2 number: "))
+#num3 = float(input("input 3 number: "))
+
+#positivesCount = (num1 > 0) + (num2 > 0) + (num3 > 0)
+
+#print(f"Cout of positive numbers: {positivesCount}")
+
+
+
+#name = "Elnur"
+#surname = "Mustafayev"
+
+#print(f"Your name is "{name}" "{surname}"")
+#print(f'Your name is "{name}" "{surname}"')
+
+
+
+
+#print("Content \n here..." + ("\n" * 3))
+#print("Content \t here...")
+#print("Content \a here...")
+
+#print("test \\\\test...")
