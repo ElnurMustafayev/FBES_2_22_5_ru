039d608e8263efd3abc4dcb0ee9e3512355e3938
Elnur Mustafayev <e.mustafayev@itstep.edu.az>
2022-09-13 21:54:40 +0400

    Add Standart Function

---
+++ Python/StandartFunctionsApp/StandartFunctionsApp/StandartFunctionsApp.py
@@ -0,0 +1,44 @@
+#text = "Hello World"
+#test = text.upper()
+#print(test == text.upper())
+
+
+
+#isMale = input("Enter 'm' for male: ").upper() == "M"
+
+#print(isMale)
+
+
+
+# 1
+#def Func(text):
+#    print(str(text).upper())
+#    return len(text)
+
+#print(Func("qwerty"))
+
+
+
+# 2
+#def IsUpper(text):
+#    return str(text).isupper()
+
+
+
+# 3
+#def Func():
+#    text = input()
+
+#    if len(text) > 10:
+#        Func()
+
+
+
+
+
+#name = "Hello test"
+
+#print(name.count("rld hell"))
+#print(name.isdecimal())
+#print(name.endswith("test"))
+#print(name.index("test"))
