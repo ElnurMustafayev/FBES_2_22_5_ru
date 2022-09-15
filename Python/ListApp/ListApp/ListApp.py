7bacf79f9b36ddb19c62739e52454a2c471ca0f0
Elnur Mustafayev <e.mustafayev@itstep.edu.az>
2022-09-15 21:48:28 +0400

    Add List

---
+++ Python/ListApp/ListApp/ListApp.py
@@ -0,0 +1,160 @@
+#name1 = "Ayxan"
+#name2 = "Emil"
+#name3 = "Asima"
+#name4 = "Tarana"
+#name5 = "Aydin"
+#name6 = "Max"
+
+#print(name1)
+#print(name2)
+#print(name3)
+#print(name4)
+#print(name5)
+#print(name6)
+
+#name7 = input("Input name: ")
+
+#print(name1)
+#print(name2)
+#print(name3)
+#print(name4)
+#print(name5)
+#print(name6)
+#print(name7)
+
+
+
+#num = 123
+#dec = 123.234
+#word = "123.45"
+
+#nums = [1,2,3,4,5,6]
+#nums = [True, False, True, True]
+#nums = [True, False, 5 > 7, not(10 == 23)]
+#nums = [123, True, "test", 46.7]
+#nums = [100 - 23, print("Test"), "a" + "b", "v".upper()]
+#nums = [len(nums), nums]
+
+#print(nums)
+
+
+
+#list1 = [1]
+#list2 = [list1]
+#list3 = [list2]
+
+#print(list3)
+
+
+
+#list3 = [[len("test"), (123 + 0.4)], [input(), "1".isdigit()]]
+
+#print(list3)
+
+#nums1 = [1,2]
+#nums2 = [3,4,5]
+#nums3 = [6]
+
+#nums = [nums1, nums2, nums3]
+#nums = [[1,2], [3,4,5], [6]]
+
+#print(len(nums))
+
+
+
+#def AddNewStudent(studentCollection):
+#    studentCollection.append(input("Enter new student's name: "))
+
+#students = [
+#    "Ayxan",
+#    "Emil",
+#    "Asima",
+#    "Tarana",
+#    "Aydin",
+#    "Max"
+#]
+
+#AddNewStudent(students)
+
+#print(students)
+
+
+#print(students)
+#students.append("qwerty")       # 7
+#students.clear()                # 0
+#students.append("qwerty")       # 1
+#students.append("qwerty")       # 2
+#print(len(students))            # print 2
+#students.append("qwerty")       # 3
+
+
+
+
+
+
+#myList = [
+#    123,4567,
+#    True, 43.457
+#]
+
+#print(not(myList[0] - myList[2]))
+
+
+#mylist = [1,2,[3,4],5]
+
+#print(mylist[3] - mylist[0])
+#print(mylist[2] - mylist[0])
+#index = 5
+#print(nums[index])
+
+
+#nums = [1,2,3,4,5]
+#print(nums[len(nums) - 1])
+
+#counter = 0
+#while counter != len(nums):
+#    print(nums[counter])
+#    counter += 1
+
+
+#def PrintList(arr):
+#    index = 0
+#    while index != len(arr):
+#        print(f"arr[{index}] = {arr[index]}")
+#        index += 1
+
+#nums = [1,2,3,4,5]
+#PrintList(nums)
+
+
+
+
+#nums = [1,2,3,4,5]
+#print(nums)
+#print(nums.pop(0))
+#print(nums)
+
+
+#def ClearArr1(arr):
+#    while len(arr) != 0:
+#        arr.pop(0)
+
+
+
+
+#def ClearArr2(arr):
+#    index = len(arr) - 1
+
+#    while index != -1:
+#        arr.pop(index)
+#        index -= 1
+
+
+
+
+#nums = [1,2,3,4,5]
+
+#print(nums)
+#ClearArr1(nums)
+#ClearArr2(nums)
+#print(nums)
