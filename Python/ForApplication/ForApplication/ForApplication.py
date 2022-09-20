d794c980637504b3e76eda73daa5619ed9c10903
Elnur Mustafayev <e.mustafayev@itstep.edu.az>
2022-09-20 21:50:26 +0400

    Add For loops

---
+++ Python/ForApplication/ForApplication/ForApplication.py
@@ -0,0 +1,138 @@
+#arr = [1, True, "test", 123.]
+
+#for x in arr:
+#    print(x)
+
+
+#print(list(range(1, 10)))
+
+#for x in True:
+#    print(x)
+
+#arr = [10 > 7, 123.35, 12 + 23, "asd"]
+
+#for item in arr:
+#    print(type(item))
+
+
+
+
+#nums = [12,34,567,8,965,46,789]
+
+#sum = 0
+#for num in nums:
+#    sum += num
+
+#print(sum)
+
+
+#counter = 0
+#sum = 0
+#while counter < len(nums):
+#    sum += nums[counter]
+#    counter += 1
+
+#print(sum)
+
+
+
+
+#range(1,10)
+
+# GetMultIn(x, y)
+
+
+#def GetMultIn(left, rigth):
+#    result = 1
+
+#    for num in range(left, rigth + 1):
+#        result *= num
+
+#    return result
+
+
+
+#mult = GetMultIn(1, 3)
+#print(mult)
+
+
+
+
+
+# range
+# 3 => 6
+#def GetFactoral(x):
+#    result = 1
+
+#    for num in range(1, x + 1):
+#        result *= num
+
+#    return result
+
+
+#print(GetFactoral(0));
+
+#myList = [1,2,3]
+#for x in myList:
+#    myList.append(1)
+
+
+#nums = [1,2,3,4,5,6,7,8]
+
+
+#def GetNums(x, y, isOdd):
+#    for x in range(x,y):
+#        if x % 2 != int(isOdd):
+#            continue
+
+#        print(x)
+
+
+
+#GetNums(10,34, True)
+
+#print("\n\n\n")
+
+#GetNums(2,6, False)
+
+
+
+
+
+
+
+
+
+# GetEvens(mylist)
+
+#def GetEvens(mylist):
+#    resultArr = []
+
+#    for x in mylist:
+#        if(x % 2 == 0):
+#            resultArr.append(x)
+
+#    return resultArr
+
+
+#nums = [1,2,3,4,5,6,7,8,9]
+#GetEvens(nums)
+#evens = GetEvens(nums)
+
+#print(nums)
+#print(evens)
+
+
+#def length(myList):
+#    count = 0
+#    for _ in myList:
+#        count += 1
+
+#    return count
+
+#mylist = [1,2,3]
+
+
+
+##len(mylist)
+#print(length(mylist))
