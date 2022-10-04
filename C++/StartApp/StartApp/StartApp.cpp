f95400a63d208451483e1606d3f59c6e5200984b
Elnur Mustafayev <e.mustafayev@itstep.edu.az>
2022-10-04 21:54:11 +0400

    Start CPP

---
+++ C++/StartApp/StartApp/StartApp.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // single-line comment
+
+    /*
+        multi-line 
+        comment
+    */
+
+    // console output
+    // cout << "Elnur!\n";
+    // console input
+    // cin >>
+
+    /*
+    
+    Text
+    Hello
+    123
+    end
+
+    */
+
+    //cout << "Text\n" << "Hello\n" << 123 << endl << "end\n";
+
+
+    // Hello, I am ...
+    // ... years old
+
+    /*
+        cout << "Hello, I am " << "Bob" << endl;
+        cout << 30 << " years old" << endl;
+    */
+
+
+
+    //cin >> "age";
+
+    /*int number = 100;
+    cout << number << endl;*/
+
+
+    /*
+    int age = 0;
+    cin >> age;
+
+    cout << "Hello, I am " << "Bob" << endl;
+    cout << "I am " << age << " years old" << endl;
+    */
+
+
+    // Input your height: ...
+    // Your height is ... sm
+
+    /*
+    int height = 0;
+    cout << "Input your height: ";
+    cin >> height;
+    cout << "Your height is " << height << " sm" << endl;
+    */
+}
