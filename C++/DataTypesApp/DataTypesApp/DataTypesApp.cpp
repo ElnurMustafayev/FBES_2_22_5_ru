014cbef04017773d25aeff5374ee1612119b6047
Elnur Mustafayev <e.mustafayev@itstep.edu.az>
2022-10-06 21:46:58 +0400

    Add Data types

---
+++ C++/DataTypesApp/DataTypesApp/DataTypesApp.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	/*unsigned long long int num = 0;
+
+	cout << (num - 1) << endl;*/
+
+	/*
+	bool check = true;
+	check = false;
+
+	cout << check << endl;
+	*/
+
+
+	/*signed short num = 0;
+
+	cout << num;*/
+
+
+	/*float num1 = 123.456;
+	double num2 = 123.456;
+
+	cout << num1 << endl;
+	cout << num2 << endl;*/
+
+	/*char symbol = '@';
+	char _symbol = '\n';
+	cout << symbol << endl;*/
+
+
+	/*char symbol;
+	cin >> symbol;
+	cout << symbol << endl;*/
+
+
+	/*char symbol = 57;
+	cout << symbol << endl;*/
+
+	/*double num1 = 234.67;
+	int num2 = num1;
+	cout << num1 << endl;
+	cout << num2 << endl;*/
+
+	
+	// input: A
+	// output: 95
+
+	/*char symbol;
+	cin >> symbol;
+	int symbolCode = symbol;
+	cout << symbolCode << endl;*/
+
+
+
+	//short code = 254;
+	//char symbol = 254;
+	//cout << symbol << endl;
+
+	/*cout << char(254);
+	cout << (char)254;
+	cout << (char)(254);
+	cout << (((char)(254)));*/
+
+	//cout << (int)'%' << endl;
+}
