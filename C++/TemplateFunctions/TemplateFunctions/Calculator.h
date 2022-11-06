b42808663a3bc98114027060faca38217beedea5
Mustafayev Elnur Ilham <mustafayev_e@itstep.edu.az>
2022-11-06 15:55:22 +0400

    Add Template Functions with Multiple Typenames

--- C++/TemplateFunctions/TemplateFunctions/Calculator.h
+++ C++/TemplateFunctions/TemplateFunctions/Calculator.h
@@ -7,6 +7,99 @@ using namespace std;
 
 
 
+
+
+// sum of types' sizes
+template<typename T1, typename T2, typename T3>
+int SumOfTypes(T1 param1, T2 param2, T3 param3) {
+	int sizeofAllParams = sizeof(param1) + sizeof(param2) + sizeof(param3);
+	return sizeofAllParams;
+}
+
+template<typename T1, typename T2, typename T3>
+int SumOfTypes() {
+	int sizeofAllTypes = sizeof(T1) + sizeof(T2) + sizeof(T3);
+	return sizeofAllTypes;
+}
+
+
+
+
+template<typename T1, typename T2, typename TResult>
+TResult MySum(T1 num1, T2 num2) {
+	return (num1 + num2);
+}
+
+template<typename T1, typename T2>
+void PrintSum(T1 num1, T2 num2) {
+	cout << (num1 + num2) << endl;
+}
+
+
+
+
+// size >= 4 -> true
+// else -> false
+template<typename T>
+void Task1() {
+	return sizeof(T) >= 4;
+}
+
+
+template<typename TVar>
+TVar Task2(int value) {
+	TVar var = value;
+	return var;
+}
+
+
+
+
+template<typename T>
+int GetSize(T param) {
+	return sizeof(T);
+}
+
+
+
+template<typename T>
+int GetSize() {
+	return sizeof(T);
+}
+
+
+
+template<typename T>
+void Func(T start) {
+	T temp = start % 5;	// will get error with T=double
+
+	cout << sizeof(start) << endl;
+
+	for (T i = start; i < 100; i++)
+	{
+		cout << sizeof(T) << ": " << i << endl;
+	}
+
+	cout << endl;
+}
+
+
+
+
+
+
+
+
+
+
+template<typename T>
+void Print(T param) {
+	cout << param << endl;
+}
+
+
+
+
 template<typename T>
 T Sum(T num1, T num2) {
 	return num1 + num2;
--- C++/TemplateFunctions/TemplateFunctions/TemplateFunctions.cpp
+++ C++/TemplateFunctions/TemplateFunctions/TemplateFunctions.cpp
@@ -3,17 +3,69 @@
 
 using namespace std;
 
-template<typename T>
-void Print(T param) {
-	cout << param << endl;
-}
-
 int main()
 {
-	Print(123);
+	/*auto money = 0.;
+	cin >> money;
+
+	cout << MySum<double, double, double>(123.23, 'a') << endl;*/
+
+	cout << SumOfTypes(123, 'a', 123.14235) << endl;
+	cout << SumOfTypes<int, char, double>() << endl;
+
+	/*PrintSum<int, int>(123, 45.56);
+	PrintSum(123, 45.56);
+	PrintSum<char, char>(123, 45.56);*/
+
+
+
+	/*cout << MySum('q', 123) << endl;
+	cout << MySum(123, 123) << endl;
+	cout << MySum(123123123123123, 123123123123123) << endl;
+	cout << MySum(123.34, '$') << endl;*/
+
+	/*string str1 = "qwe";
+	string str2 = "rty";
+
+	cout << MySum(str1, str2) << endl;*/
+
+
+
+	//Task1<int>();
+	//Task1<char>();
+
+	//char result = Task2<char>(97);
+	//cout << sizeof((Task2<double>(97) + Task2<int>(100))) << endl;;
+
+
+
+	/*cout << GetSize('a') << endl;
+	cout << GetSize(true) << endl;
+	cout << GetSize(145465) << endl;
+	cout << GetSize(123.2f) << endl;
+	cout << GetSize(12312313123123123) << endl;
+	cout << GetSize(123.2) << endl;
+
+	cout << "\n\n\n" << endl;
+
+	cout << GetSize<char>() << endl;
+	cout << GetSize<bool>() << endl;
+	cout << GetSize<int>() << endl;
+	cout << GetSize<float>() << endl;
+	cout << GetSize<long long>() << endl;
+	cout << GetSize<double>() << endl;*/
+
+
+
+	/*Func(87);
+	Func(87.5);
+	Func('A');*/
+	//Func("tqwetquwyet");
+
+	/*Print(123);
 	Print(123.234);
 	Print(true);
-	Print("sdasghdahsd");
+	Print("sdasghdahsd");*/
 
 
 	//int result = Sum(123, 277);
