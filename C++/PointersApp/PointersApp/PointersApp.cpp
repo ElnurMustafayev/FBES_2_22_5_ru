492df8938ef4fce734e6248cbbe0b7a918d2e1e0
Mustafayev Elnur Ilham <mustafayev_e@itstep.edu.az>
2022-11-10 21:52:10 +0400

    Add Pointers

---
+++ C++/PointersApp/PointersApp/PointersApp.cpp
@@ -0,0 +1,126 @@
+#include <iostream>
+
+using namespace std;
+
+//void ChangeNumber(int* num) {
+//	*num = 777;
+//}
+
+
+
+
+//void IncrementNumber(int* num, int value) {
+//	num += value;
+//}
+
+
+
+
+int main()
+{
+	/*int num = 100;
+	int value = 100;
+
+	IncrementNumber(&num, value);
+
+	cout << num << endl;*/
+
+	/*int number = 123;
+
+	ChangeNumber(&number);
+
+	cout << number << endl;*/
+
+	// IncrementNumber(num, 100)
+
+	/*int num = 100;
+
+	int* numPtr = &num;
+
+	cout << numPtr << endl;*/
+
+	/*double num = 123.45;
+	double* numPtr = &num;
+
+	cout << numPtr << endl;
+	cout << &num << endl;*/
+
+	/*bool test = true;
+	bool* stackStartAddress = &test;
+	cout << stackStartAddress << endl;*/
+
+
+	/*int num = 100;
+
+	int* numPtr = &num;
+
+	*numPtr = 777;
+
+	cout << *numPtr << endl;
+	cout << num << endl;*/
+
+
+
+	/*int num = 100;
+
+	int* numPtr1 = &num;
+	int* numPtr2 = &num;
+	int* numPtr3 = &num;
+
+	*numPtr1 = 777;
+
+	cout << *numPtr2 << endl;
+	cout << *numPtr3 << endl;
+	cout << num << endl;*/
+
+
+
+	/*int num1 = 10, num2 = 100;
+
+	int* numPtr = &num1;
+	cout << numPtr << endl;
+
+	numPtr = &num2;
+	cout << numPtr << endl;*/
+
+
+
+	//int num = 100;
+	//int* numPtr = &num;
+
+	//numPtr = nullptr;
+	////numPtr = &num;
+	//
+	//*numPtr += 50;
+
+	//cout << num << endl;
+
+	/*cout << numPtr << endl;
+	numPtr = nullptr;
+	cout << numPtr << endl;*/
+
+
+
+	/*int num = 0;
+	int* numPtr = NULL;*/
+
+
+	
+	
+	//int num = 100;
+	//int* numPtr = &num;
+	//num += 10;
+	//cout << *numPtr << endl;
+
+
+	/*int num = 10;
+
+	int* numPtr = &num;
+
+	*numPtr += ++num;
+	*numPtr += num;
+
+	cout << *numPtr << endl;
+	cout << num << endl;*/
+
+}
