d1610f6ad885cb132cd1e9f486a746f2d9aa66bc
Mustafayev Elnur Ilham <mustafayev_e@itstep.edu.az>
2022-11-22 21:42:43 +0400

    Add mult arrays

--- C++/MultipleArrayApp/MultipleArrayApp/MultipleArrayApp.cpp
+++ C++/MultipleArrayApp/MultipleArrayApp/MultipleArrayApp.cpp
@@ -1,18 +1,78 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <iostream>
 
 using namespace std;
 
+void PrintDoubleArray1(int arr[][3], const int arraysCount, const int elementsCount) {
+    for (int y = 0; y < arraysCount; y++)
+    {
+        for (int x = 0; x < elementsCount; x++)
+        {
+            cout << arr[y][x] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void PrintDoubleArray2(int arr[][3], const int rows, const int columns) {
+    int* start = arr[0];
+
+    for (int y = 0; y < rows; y++)
+    {
+        for (int x = 0; x < columns; x++)
+        {
+            cout << *(start + (y * columns) + x) << "\t";
+        }
+        cout << endl;
+    }
+}
+
+
+void PrintDoubleArray3(int* start, const int rows, const int columns) {
+    /*for (int i = 0; i < (rows * columns); i++)
+    {
+        if (i % columns == 0 && i >= columns)
+            cout << endl;
+
+        cout << *(start + i) << "\t";
+    }*/
+
+    for (int y = 0; y < rows; y++)
+    {
+        int* currentArr = (start + (y * columns));
+        for (int x = 0; x < columns; x++)
+        {
+            cout << currentArr[x] << "\t";
+        }
+        cout << endl;
+    }
+}
+
 int main()
 {
+    /*const int rows = 4;
+    const int columns = 3;
+
+    int duoArray[rows][columns]{
+        {1,2,5},
+        {3,4,6},
+        {7,8,9},
+        {12,24,45}
+    };
+
+    PrintDoubleArray3(*duoArray, rows, columns);*/
+
+
     //int arr[5];
 
-    const int xLength = 3, yLength = 3;
+    /*const int xLength = 3, yLength = 3;
 
     int duoArray[3][3]{
         {1,2,5},
         {3,4},
         {}
-    };
+    };*/
 
     /*int tetraArr[3][3][3]{
 
@@ -42,4 +102,42 @@ int main()
     //cout << duoArray[2][2] << endl;
 
     //cout << sizeof(duoArray) << endl;
+
+
+
+    /*int duoArray[3][3]{
+        {1,2,5},
+        {3,4,6},
+        {7,8,9}
+    };*/
+
+    /*int* ptr = duoArray[0];
+    cout << ptr[1] << endl;*/
+
+    /*cout << duoArray << endl;
+    cout << duoArray[0] << endl;
+    cout << duoArray[1] << endl;
+    cout << duoArray[2] << endl;*/
+
+
+
+
+    /*int arr[1][1][1];
+
+    cout << arr << endl;
+    cout << arr[0] << endl;
+    cout << arr[0][0] << endl;*/
+
+
+    char dest[50] = "Hello";
+    char source[20] = "World";
+
+    //strcpy(dest, source);
+
+    /*strcat(dest, " ");
+    strcat(dest, source);*/
+
+    //cout << dest << endl;
+
+    //cout << strlen(dest) << endl;   // 5
 }
