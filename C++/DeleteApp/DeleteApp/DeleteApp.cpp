3db95def432f65bdea42bfa5be2ccf7b5ce42edc
Mustafayev Elnur Ilham <mustafayev_e@itstep.edu.az>
2022-12-13 21:58:02 +0400

    Add delete operation

---
+++ C++/DeleteApp/DeleteApp/DeleteApp.cpp
@@ -0,0 +1,142 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <Windows.h>
+#include <conio.h>
+
+using namespace std;
+
+
+//int* GetArray(int length) {
+//    int* arr = new int[length] {};
+//
+//    return arr;
+//}
+//
+//int* GetVariable(int value) {
+//    int* var = new int(value);
+//
+//    return var;
+//}
+
+
+
+//int* ChangeNumber(int* num) {
+//    *num += 100;
+//}
+
+
+
+//char* MonsterFunction(char arr[]) {
+//    return arr;
+//}
+
+
+
+int main()
+{
+    /*char* history = new char[500] {};
+
+    strcat(history, "one\n");
+    strcat(history, "two\n");
+    strcat(history, "three\n");
+    strcat(history, "four\n");
+    strcat(history, "five\n");
+
+    cout << history << endl;*/
+
+
+
+    /*char text[] = "text";
+    MonsterFunction(text);*/
+
+
+
+    /*int num = 123;
+    ChangeNumber(&num);
+    cout << num << endl;*/
+
+
+
+
+    /*int num1 = 0;
+
+    int* ptr1 = &num1;
+    int* ptr2 = &num1;
+    int* ptr3 = &num1;
+    int* ptr4 = ptr2;
+
+    (*ptr1) += 10;
+    (*ptr2) += 10;
+    (*ptr3) += 10;
+    (*ptr4) += 10;
+    num1 += 10;
+
+    cout << num1 << endl;*/
+
+
+    /*num2 += 100;
+
+    cout << num1 << endl;*/
+
+
+    /*int result1 = 123;
+
+    ChangeNumber(result1);
+
+    cout << result1 << endl;*/
+
+
+
+
+
+
+
+    /*int* num = new int[123];
+
+    cout << num << endl;
+    delete[] num;
+    cout << num << endl;*/
+
+
+    /*int* arr = GetArray(10);
+    delete[] arr;
+
+    int* var = GetVariable(123);
+    delete var;*/
+
+
+    /*cout << "Start" << endl;
+
+    for (int i = 0; i < 10000; i++)
+    {
+        Sleep(10);
+        int* arr = new int[1000000]{};
+        delete[] arr;
+    }
+
+    cout << "End" << endl;
+    int temp = _getch();*/
+
+
+    /*std::cout << "Start" << endl;
+
+    int* temp = new int(123);
+    temp = nullptr;
+
+    delete temp;
+
+    std::cout << "End" << endl;*/
+    
+
+
+    /*int* arr = new int[10] {};
+
+    delete[10] arr;*/
+
+
+
+
+    
+
+}
