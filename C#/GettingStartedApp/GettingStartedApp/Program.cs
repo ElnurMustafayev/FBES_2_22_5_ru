c8dd763cc4c64bbe08b54eb341003326839042a5
Mustafayev Elnur Ilham <elnur.mustafayeev@gmail.com>
2023-04-04 21:43:37 +0400

    Add C#

---
+++ C#/GettingStartedApp/GettingStartedApp/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace GettingStartedApp {
+    class Program {
+
+        static void Main() {
+            //long l;
+            //int num1 = 111;
+            //uint num2 = 222;
+            //short s1 = 1;
+            //ushort s2 = 2;
+            //byte b1 = 3;    // unsigned [0 - 255]
+            //sbyte b2 = 3;   // signed
+            //bool b = true;
+
+            //decimal amount = 13.3m;
+
+            //double num = 1_000_000_000.5;
+
+            //Console.Write(num);
+            //Console.Write(' ');
+            //Console.Write(123_000);
+        }
+    }
+}
