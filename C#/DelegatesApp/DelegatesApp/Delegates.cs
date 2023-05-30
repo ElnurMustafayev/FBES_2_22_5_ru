e5b4b60852ab4bcf35b70a02e665677fbecc7231
Mustafayev Elnur Ilham <elnur.mustafayeev@gmail.com>
2023-05-30 21:57:19 +0400

    Add Lambdas

---
+++ C#/DelegatesApp/DelegatesApp/Delegates.cs
@@ -0,0 +1,132 @@
+﻿using static DelegatesApp.Delegates;
+
+namespace DelegatesApp {
+    public class Delegates {
+        public int Num = 123;
+
+        public delegate void MyDelegate();
+        public delegate bool NumberCheck(int num);
+        public delegate bool CheckerDel<T>(T param);
+
+
+        public void ShowNum() => Console.WriteLine(this.Num);
+
+
+        public static void SayHello() => Console.WriteLine("Hello World!");
+        public static void SayGoodbye() => Console.WriteLine("Goodbye World!");
+        public static bool IsPositive(int num) {
+            Console.WriteLine("IsPositive");
+            return num > 0;
+        }
+        public static bool IsNegative(int num) {
+            Console.WriteLine("IsNegative");
+            return num < 0;
+        }
+
+        //public static List<int> GetPositiveNumbers(List<int> numbers) {
+        //    var positiveNumbers = new List<int>();
+
+        //    foreach (var number in numbers) {
+        //        if(number > 0)
+        //            positiveNumbers.Add(number);
+        //    }
+
+        //    return positiveNumbers;
+        //}
+
+        //public static List<int> GetNegativeNumbers(List<int> numbers) {
+        //    var positiveNumbers = new List<int>();
+
+        //    foreach (var number in numbers) {
+        //        if (number < 0)
+        //            positiveNumbers.Add(number);
+        //    }
+
+        //    return positiveNumbers;
+        //}
+
+
+        public static List<T> GetNumbers<T>(List<T> numbers, CheckerDel<T> numberCheck) {
+            var resultNumber = new List<T>();
+
+            foreach (T number in numbers) {
+                if (numberCheck.Invoke(number))
+                    resultNumber.Add(number);
+            }
+
+            return resultNumber;
+        }
+
+        public delegate T2 MyGenericDel<T1, T2, T3>(T1 param1, T3 param2);
+
+        public static double Test(int num, bool check) {
+            Console.WriteLine("Test");
+            return 0;
+        }
+
+        public static double qwerty(int num, bool check) {
+            Console.WriteLine("qwerty");
+            return 0;
+        }
+
+        static void Mainnn() {
+            if(false) {
+                MyGenericDel<int, double, bool> del = Test;
+                del += qwerty;
+                del += Test;
+                del += qwerty;
+                del += qwerty;
+                del -= qwerty;
+
+                del.Invoke(123, true);
+            }
+
+
+            if(false) {
+                var numbers = new List<int>() {
+                    12,0,-35,46,-2
+                };
+                var positiveNumbers = GetNumbers<int>(numbers, IsPositive);
+
+                foreach (var number in positiveNumbers) {
+                    Console.WriteLine(number);
+                }
+            }
+
+
+
+            if(false) {
+                CheckerDel<int> del = IsPositive;
+            }
+
+
+
+
+            if(false) {
+                var obj = new Delegates();
+                MyDelegate del = obj.ShowNum;
+
+                del.Invoke();
+            }
+
+            if (false) {
+                NumberCheck numberCheck = IsNegative;
+                numberCheck += IsPositive;
+
+                bool result = numberCheck.Invoke(15);
+
+                Console.WriteLine(result);
+            }
+
+            if(false) {
+                MyDelegate del1 = SayHello;
+                del1 += SayGoodbye;
+
+                del1.Invoke();
+                del1();
+
+                NumberCheck del2 = IsPositive;
+            }
+        }
+    }
+}
---
+++ C#/DelegatesApp/DelegatesApp/Lambdas.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections.Generic;
+
+namespace DelegatesApp;
+
+public class Lambdas {
+    public delegate void FuncDel();
+    public delegate void FuncDel<T>(T obj);
+    public delegate void FuncDel<T1, T2>(T1 obj1, T2 obj2);
+
+    public static void DoSomething1() {
+        Console.WriteLine($"Something");
+    }
+    public static void DoSomething2(int num) => Console.WriteLine($"Result: {num}");
+    public static void DoSomething3(int num1, double num2) {
+        Console.WriteLine($"Result: {num1} {num2}");
+    }
+
+    //public static void DoAction(Action<int> action1, Action<bool> action2, Action<string, string> action3) {
+    //    action2.Invoke(true);
+    //    action1.Invoke(12);
+    //    action3.Invoke("qwe", "qweqwe");
+    //}
+
+    public static bool CheckName(string name) {
+        if (string.IsNullOrEmpty(name))
+            return false;
+
+        foreach (char symbol in name) {
+            if (char.IsLetter(symbol) == false)
+                return false;
+        }
+
+        return true;
+    }
+
+    public static int Minus(string first, string second) {
+        if (string.IsNullOrWhiteSpace(first) || string.IsNullOrWhiteSpace(second))
+            return 0;
+
+        return first.Length - second.Length;
+    }
+
+    public static void Main() {
+        if(false) {
+            //Action action1 = () => Console.WriteLine("Hello World");
+            //action1.Invoke();
+
+            //Action<int, double> action2 = (x, y) => Console.WriteLine(x);
+            //action2.Invoke(1, 2);
+
+            //Func<int, string> GetNumberAsString = (num) => num.ToString();
+
+            //var numberStr = GetNumberAsString.Invoke(123);
+            //Console.WriteLine(numberStr);
+
+
+            Func<string, string, int> MinusForStrings = (word1, word2) => {
+                if (string.IsNullOrWhiteSpace(word1) || string.IsNullOrWhiteSpace(word2))
+                    return 0;
+
+                return word1.Length - word2.Length;
+            };
+
+            var result = MinusForStrings.Invoke("text", "qwerty");
+            Console.WriteLine(result);
+        }
+
+
+
+
+        if(false) {
+            Action<string> action = delegate (string text) {
+                Console.WriteLine("Anonimous method!");
+                Console.WriteLine($"Message: \"{text}\"");
+            };
+
+            action.Invoke("Test");
+
+
+
+            Predicate<int> isPositive = delegate (int num) {
+                return num > 0;
+            };
+
+            var result = isPositive.Invoke(0);
+            Console.WriteLine(result);
+
+
+
+            Func<decimal, int, string> func = delegate (decimal num, int count) {
+                return $"{num}-{count}";
+            };
+
+            Console.WriteLine(func.Invoke(12,45));
+        }
+
+        if(false) {
+            //Action<int> action1 = new Action<int>(DoSomething2);
+            //Action<int> action2 = DoSomething2;
+
+            Action action3 = delegate () {
+                Console.WriteLine("The First");
+            };
+
+            action3 += delegate () {
+                Console.WriteLine("The Second");
+            };
+
+            action3.Invoke();
+        }
+
+        if(false) {
+            Func<string, string, int> func = Minus;
+            var result = func.Invoke("test", "    ");
+            Console.WriteLine(result);
+
+            Func<string, bool> func1 = CheckName;
+            Predicate<string> predicate = CheckName;
+        }
+
+        if(false) {
+            Predicate<string> predicate = CheckName;
+            Console.WriteLine(predicate.Invoke("test"));
+            Console.WriteLine(predicate.Invoke("bob777"));
+            Console.WriteLine(predicate.Invoke("qw2erty**"));
+        }
+
+        if(false) {
+            // Action
+
+            //Action action = DoSomething1;
+            //Action<int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int> action;
+
+            //Action<int, double> action = DoSomething3;
+        }
+
+
+        //FuncDel<int> action = DoSomething2;
+    }
+}
