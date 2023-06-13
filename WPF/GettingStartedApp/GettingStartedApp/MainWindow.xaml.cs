81adc56f1d24d8b31bfdb13e1e0f03d506189cc5
Mustafayev Elnur Ilham <elnur.mustafayeev@gmail.com>
2023-06-13 21:51:08 +0400

    Add WPF

---
+++ WPF/GettingStartedApp/GettingStartedApp/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows;
+
+namespace GettingStartedApp {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+
+            //this.MyButton.MaxWidth = 500;
+            //this.MyButton.Width = 300;
+
+            //this.MyTextBox.Style
+
+            this.MyTextBox.Text = Guid.NewGuid().ToString();
+        }
+    }
+}
