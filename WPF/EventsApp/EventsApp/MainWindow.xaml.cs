6da17401ad69c1cbd7bc13b1ff2d590a59092ef1
Mustafayev Elnur Ilham <elnur.mustafayeev@gmail.com>
2023-06-15 18:40:31 +0400

    Add WPF events

---
+++ WPF/EventsApp/EventsApp/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EventsApp {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+
+            this.ClickMeButton.Background = new SolidColorBrush(Colors.Red);
+
+            this.ClickMeButton.Click += (sender, evengArgs) => {
+                Console.WriteLine($"Sender Type: {sender.GetType()}");
+            };
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            this.ClickMeButton.Content = Guid.NewGuid().ToString();
+            Console.WriteLine("Button content updated!");
+        }
+
+        private void MyTextBox_TextChanged(object sender, TextChangedEventArgs e) {
+            if(sender is TextBox textbox) {
+                if (string.IsNullOrWhiteSpace(textbox.Text))
+                    return;
+
+                this.ClickMeButton.Content = textbox.Text.Trim();
+            }
+        }
+
+        private void CheckBox_Check(object sender, RoutedEventArgs e) {
+            if(sender is CheckBox checkBox) {
+                Console.WriteLine(checkBox.IsChecked);
+                this.ClickMeButton.Visibility = checkBox.IsChecked == true ? Visibility.Visible : Visibility.Hidden;
+                this.MyTextBox.Visibility = checkBox.IsChecked == true ? Visibility.Visible : Visibility.Hidden;
+            }
+        }
+    }
+}
