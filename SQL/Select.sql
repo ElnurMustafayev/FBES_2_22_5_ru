7a2e74071ea6b84b0d1034c13d51975a99299bce
Mustafayev Elnur Ilham <mustafayev_e@itstep.edu.az>
2023-08-24 21:46:01 +0400

    Add SQL

---
+++ SQL/Select.sql
@@ -0,0 +1,47 @@
+--select u.Gender
+--from Users u
+
+--select Users.Gender
+--from Users
+
+--select TestDb.dbo.Users.Gender
+--from Users
+
+
+
+--select top 1 [Name]
+--from Users
+
+--select top 2 *
+--from Users
+
+--select [Name] + ' ' + [Surname] as 'Fullname'
+--from Users
+
+--select 'Mr. ' + [Name]
+--from Users
+
+--select [Age] + 100
+--from Users
+
+--select 45 + 15
+--from Users
+
+--select 'Hello World' as 'text'
+--from Users
+
+--select [Name] as 'Имя', [Surname] as 'Фамилия'
+--from Users
+
+--select [Name] as 'Ad', [Surname] as 'Soyad' 
+--from Users
+
+--select [Name], [Surname] from Users
+
+--select [Name]
+--from Users
+
+--select * from Users
+
+--select *
+--from Users
