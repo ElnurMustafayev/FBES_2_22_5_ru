668a0f77f40e9324464785beb5e9fdc4e5005a03
Mustafayev Elnur Ilham <mustafayev_e@itstep.edu.az>
2023-09-17 14:33:21 +0400

    Add Sql Injection

--- Design Patterns/RepositoryApp/Sql/Creates.sql
+++ Design Patterns/RepositoryApp/Sql/Creates.sql
@@ -13,11 +13,10 @@ create table Users (
 insert into Users(Name, Age) 
 values('John', null)
 
-select * from Users
+-- select * from Users
 
-
-if not exists (select * from sysobjects where name='cars' and xtype='U')
-    create table cars (
-        Name varchar(64) not null
-    )
-go
+-- if not exists (select * from sysobjects where name='cars' and xtype='U')
+--     create table cars (
+--         Name varchar(64) not null
+--     )
+-- go
---
+++ ORM/Ado.net/ParametersApp/ParametersApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using ParametersApp.Entities;
+using ParametersApp.Repositories;
+using System.Data.SqlClient;
+
+
+
+
+
+
+const string connectionString = $"Server=localhost;Database=TestDb;User Id=admin;Password=admin;";
+
+using var sqlConnection = new SqlConnection(connectionString);
+sqlConnection.Open();
+
+Console.Write("Name: ");
+string name = Console.ReadLine();
+
+Console.Write("Age: ");
+int age = int.TryParse(Console.ReadLine(), out int ageInput) ? ageInput : -1;
+
+
+var command = new SqlCommand(
+    cmdText: @$"insert into Users(Name, Age) 
+                values('{name}', {age})",
+    sqlConnection);
+
+command.ExecuteNonQuery();
+
+/*
+const string connectionString = $"Server=localhost;Database=TestDb;User Id=admin;Password=admin;";
+var userRepository = new UserRepository(connectionString);
+
+User newUser = new User()
+{
+    Name = "Emil",
+    Age = 21
+};
+
+userRepository.Create(newUser);
+
+
+var users = userRepository.GetAll();
+
+foreach (var user in users)
+{
+    System.Console.WriteLine(user);
+}
+
+*/
