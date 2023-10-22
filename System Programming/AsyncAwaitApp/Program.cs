989746e093abbae589259d96a6c2a948a6cc32e4
Mustafayev Elnur Ilham <mustafayev_e@itstep.edu.az>
2023-10-22 16:09:26 +0400

    Add Task, async/await, StateMachine

---
+++ System Programming/AsyncAwaitApp/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace AsyncAwaitApp;
+
+public class Program
+{
+    public static Task MyMethodAsync()
+    {
+        var task = new Task(() =>
+        {
+            Thread.Sleep(3000);
+            Console.WriteLine("Hello World!");
+        });
+
+        task.Start();
+
+        return task;
+    }
+
+    //public static async Task MyClientCode()
+    //{
+    //    await MyMethodAsync();
+    //}
+
+    public static Task Func(int ms)
+    {
+        return Task.Run(() =>
+        {
+            Thread.Sleep(ms);
+            Console.WriteLine(ms);
+        });
+    }
+
+    public static Task<int> Func(int ms, int result)
+    {
+        return Task.Run(() =>
+        {
+            Thread.Sleep(ms);
+            Console.WriteLine(ms);
+
+            return result;
+        });
+    }
+
+    public static async Task AsyncFunc()
+    {
+        Console.WriteLine("start");
+        await Func(2000);
+        Console.WriteLine("middle");
+        var result = await Func(1000, 777);
+        Console.WriteLine("value: " + result);
+        Console.WriteLine("middle");
+        await Func(3000);
+        Console.WriteLine("end");
+    }
+
+    public static async Task Main()
+    {
+        AsyncFunc();
+
+        while(true)
+        {
+            Thread.Sleep(200);
+            Console.WriteLine("Main");
+        }
+
+
+
+        //Console.WriteLine("Start");
+        //await MyMethodAsync();
+        //Console.WriteLine("End");
+
+        //var task = MyMethodAsync();
+        //task.Wait();
+    }
+}
---
+++ System Programming/TasksApp/Program.cs
@@ -0,0 +1,176 @@
+﻿namespace MyNamespace;
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading;
+
+public class User
+{
+    [JsonPropertyName("name")]
+    public string? Name { get; set; }
+
+    [JsonPropertyName("surname")]
+    public string? Surname { get; set; }
+}
+
+public class Program
+{
+    public static Task<int> GetUsersCountAsync()
+    {
+        return Task<int>.Run(() =>
+        {
+            var usersJson = File.ReadAllText("assets/users.json");
+            var users = JsonSerializer.Deserialize<IEnumerable<User>>(usersJson);
+            Thread.Sleep(2000);
+            return users?.Count() ?? 0;
+        });
+    }
+
+    public static int GetUsersCount()
+    {
+        int result = 0;
+
+        var thread = new Thread(() =>
+        {
+            var usersJson = File.ReadAllText("assets/users.json");
+            var users = JsonSerializer.Deserialize<IEnumerable<User>>(usersJson);
+            Thread.Sleep(2000);
+            var count = users?.Count() ?? 0;
+            result = count;
+        });
+
+        thread.Start();
+        thread.Join();
+
+        return result;
+    }
+
+    public static Task CountAsync(int start, int end)
+    {
+        //var task = new Task(() =>
+        //{
+        //    for (int i = start, j = 1; i < end; i++, j++)
+        //    {
+        //        Console.WriteLine($"{j}. {i}");
+        //        Thread.Sleep(1000);
+        //    }
+        //});
+
+        //task.Start();
+
+        //return task;
+        return Task.Run(() =>
+        {
+            for (int i = start, j = 1; i < end; i++, j++)
+            {
+                Console.WriteLine($"{j}. {i}");
+                Thread.Sleep(1000);
+            }
+        });
+    }
+
+    public static void Main()
+    {
+        //var task = Task.Run(() =>
+        //{
+        //    Console.WriteLine("1 start");
+        //    Thread.Sleep(1000);
+        //    Console.WriteLine("1 end");
+        //});
+
+        //task.ContinueWith((t) =>
+        //{
+        //    Console.WriteLine("2 start");
+        //    Thread.Sleep(2000);
+        //    Console.WriteLine("2 end");
+        //});
+
+        //Console.ReadKey();
+
+        //task.Start();
+
+
+
+
+
+        //var tasks = new Task[]
+        //{
+        //    Task.Run(() =>
+        //    {
+        //        Thread.Sleep(1000);
+        //        Console.WriteLine("1");
+        //    }),
+        //    Task.Run(() =>
+        //    {
+        //        Thread.Sleep(2000);
+        //        Console.WriteLine("2");
+        //    }),
+        //    Task.Run(() =>
+        //    {
+        //        Thread.Sleep(5000);
+        //        Console.WriteLine("5");
+        //    })
+        //};
+
+        //Task.WaitAll(tasks);
+        //Task.WaitAny(tasks);
+
+
+
+        //var result = Task.Run(() => 123.ToString());
+
+        //Console.WriteLine(result.Result);
+
+
+
+
+
+        //var randomizerTask = new Task<int>(() =>
+        //{
+        //    Thread.Sleep(1000);
+        //    Console.WriteLine("Test");
+        //    return Random.Shared.Next(700, 800);
+        //});
+        //randomizerTask.Start();
+        ////randomizerTask.Wait();
+
+        //Console.WriteLine(randomizerTask.Result);
+        //Console.WriteLine(randomizerTask.Result);
+        //Console.WriteLine(randomizerTask.Result);
+
+
+        //randomizerTask.Wait();
+
+
+
+
+        //var task = CountAsync(5, 10);
+
+        //task.Start();
+        //task.Wait();
+
+
+
+        //var task = new Task(() =>
+        //{
+        //    Thread.Sleep(100);
+        //    Console.WriteLine("Hello from Task 1!");
+        //    Thread.Sleep(100);
+        //    Console.WriteLine("Hello from Task 2!");
+        //    Thread.Sleep(100);
+        //    Console.WriteLine("Hello from Task 3!");
+        //});
+
+        //task.RunSynchronously();
+
+        //task.Start();
+        //task.Wait();
+
+        //Console.ReadKey();
+
+
+
+        //var count = GetUsersCount();
+        //Console.WriteLine($"Users' count: {count}");
+    }
+}
