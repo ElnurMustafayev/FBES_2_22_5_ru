1aa2db87169d5e70692de00441e59e641c9146ce
Mustafayev Elnur Ilham <mustafayev_e@itstep.edu.az>
2023-10-31 21:52:30 +0400

    Add Sockets

---
+++ Network Programming/ServerApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+const string serverIp = "127.0.0.1";
+const int port = 8080;
+
+IPAddress address = IPAddress.Parse(serverIp);
+IPEndPoint endPoint = new IPEndPoint(address, port);
+
+
+
+Socket serverSocket = new Socket(
+    addressFamily: AddressFamily.InterNetwork,
+    socketType: SocketType.Stream,
+    protocolType: ProtocolType.Tcp);
+
+serverSocket.Bind(endPoint);
+serverSocket.Listen(backlog: 5);
+
+Console.WriteLine($"Server started on {serverIp}:{port}");
+
+Socket clientSocket = await serverSocket.AcceptAsync();
+
+byte counter = 0;
+while(true)
+{
+    counter++;
+    await clientSocket.SendAsync(new byte[] { counter, counter, counter });
+
+    Thread.Sleep(1000);
+}
