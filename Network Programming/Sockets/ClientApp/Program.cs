e1e6c0772670ebe3357949f502d52900a2011c6b
Mustafayev Elnur Ilham <mustafayev_e@itstep.edu.az>
2023-11-02 21:29:43 +0400

    End Sockets

---
+++ Network Programming/Sockets/ClientApp/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+const string serverIp = "127.0.0.1";
+const int port = 8080;
+
+IPAddress address = IPAddress.Parse(serverIp);
+IPEndPoint endPoint = new IPEndPoint(address, port);
+
+Socket clientSocket = new Socket(
+    addressFamily: AddressFamily.InterNetwork,
+    socketType: SocketType.Stream,
+    protocolType: ProtocolType.Tcp);
+
+await clientSocket.ConnectAsync(endPoint);
+
+
+
+byte[] buffer = new byte[1024];
+ThreadPool.QueueUserWorkItem(async (obj) =>
+{
+    try
+    {
+        while (true)
+        {
+            var size = await clientSocket.ReceiveAsync(buffer);
+
+            //if (size == 0)
+            //{
+            //    Console.WriteLine($@"Disconnected by Server!");
+            //    break;
+            //}
+
+            var msg = Encoding.Unicode.GetString(buffer, 0, size);
+            Console.WriteLine($@"Server: ""{msg}""");
+        }
+    }
+    catch (SocketException)
+    {
+        Console.WriteLine("Disconnected from server!");
+        Environment.Exit(0);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("System error: " + ex);
+    }
+}, null);
+
+
+
+try
+{
+    Console.WriteLine("Write message and press 'enter' to send...\n\n");
+    while (true)
+    {
+        string message = Console.ReadLine() ?? string.Empty;
+        var messageInBytes = Encoding.Unicode.GetBytes(message);
+
+        await clientSocket.SendAsync(messageInBytes);
+    }
+}
+catch (SocketException)
+{
+    Console.WriteLine("Disconnected from server!");
+    Environment.Exit(0);
+}
+catch (Exception ex)
+{
+    Console.WriteLine("System error: " + ex);
+}
+finally
+{
+    clientSocket.Dispose();
+}
---
+++ Network Programming/Sockets/ServerApp/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+const string serverIp = "127.0.0.1";
+const int port = 8080;
+
+IPAddress address = IPAddress.Parse(serverIp);
+IPEndPoint endPoint = new IPEndPoint(address, port);
+
+
+
+Socket serverSocket = new Socket(
+    addressFamily: AddressFamily.InterNetwork,
+    socketType: SocketType.Stream,
+    protocolType: ProtocolType.Tcp);
+
+serverSocket.Bind(endPoint);
+serverSocket.Listen(backlog: 5);
+
+Console.WriteLine($"Server started on {serverIp}:{port}");
+
+while(true)
+{
+    Socket clientSocket = await serverSocket.AcceptAsync();
+    Console.WriteLine($"Client was connected {clientSocket.RemoteEndPoint}");
+
+    ThreadPool.QueueUserWorkItem(async (socket) => {
+        try
+        {
+            var buffer = new byte[1024];
+            while (true)
+            {
+                var size = await socket.ReceiveAsync(buffer);
+                var requestMessage = Encoding.Unicode.GetString(buffer, 0, size);
+
+                Console.WriteLine($@"{socket.RemoteEndPoint}: ""{requestMessage}""");
+
+                if(requestMessage.ToLower() == "hello")
+                {
+                    var responseMessage = "Hi";
+                    var responseMessageInBytes = Encoding.Unicode.GetBytes(responseMessage);
+                    await socket.SendAsync(responseMessageInBytes);
+
+                    Thread.Sleep(1000);
+                    //socket.Shutdown(SocketShutdown.Both);
+                    //socket.Close();
+                    //await socket.DisconnectAsync(false);
+                    break;
+                }
+            }
+        }
+        catch (SocketException)
+        {
+            Console.WriteLine($"Client {socket.RemoteEndPoint} disconnected!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"System error in {socket.RemoteEndPoint} client.\n{ex}");
+        }
+    }, clientSocket, false);
+}
