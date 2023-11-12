3bdd70526e873e48262135bea5b76adc7fa96f7a
Elnur Mustafayev <elnur.mustafayeev@gmail.com>
2023-11-12 16:02:56 +0400

    Add Udp client-server app

---
+++ Network Programming/UDP/ReceiverApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+UdpClient client = new UdpClient(8080);
+System.Console.WriteLine("Udp Client started!");
+const string endMessage = "Image sent!";
+var endMessageInBytes = Encoding.Unicode.GetBytes(endMessage);
+
+List<byte> imageInBytes = new List<byte>(capacity: 1024);
+
+while(true) {
+    for (int i = 0; true; i++)
+    {
+        var result = await client.ReceiveAsync();
+        var messageInBytes = result.Buffer;
+
+        if(messageInBytes.Length == endMessageInBytes.Length 
+            && Encoding.Unicode.GetString(messageInBytes) == endMessage) {
+            var filename = $"Data/{Guid.NewGuid()}.jpg";
+
+            File.WriteAllBytes(filename, imageInBytes.ToArray());
+            System.Console.WriteLine($"{filename} created! packets count: {i}");
+            break;
+        }
+
+        imageInBytes.AddRange(messageInBytes);
+
+        //System.Console.WriteLine($"Packet #{i} received!");
+    }
+}
+
+
+
+//while(true) {
+    //File.WriteAllBytes($"Data/{Guid.NewGuid()}.png", result.Buffer);
+    //System.Console.WriteLine($"client '{result.RemoteEndPoint}' sent file");
+
+
+
+    //var messageInBytes = result.Buffer;
+    // var messageTxt = Encoding.Unicode.GetString(messageInBytes);
+    // System.Console.WriteLine($"{result.RemoteEndPoint}: {messageTxt}");
+//}
---
+++ Network Programming/UDP/SenderApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+UdpClient client = new UdpClient(8000);
+const int packetLimit = 10_000; 
+
+const string endMessage = "Image sent!";
+var endMessageInBytes = Encoding.Unicode.GetBytes(endMessage);
+
+while(true) {
+    var imageInBytes = await File.ReadAllBytesAsync("Data/cat3.jpg");
+
+    for (int packetCounter = 0; true; packetCounter++)
+    {
+        byte[] currentPacketInBytes = imageInBytes
+            .Skip(packetCounter * packetLimit)
+            .Take(packetLimit)
+            .ToArray();
+
+        if(currentPacketInBytes.Length == 0) {
+            await client.SendAsync(endMessageInBytes, "localhost", 8080);
+            System.Console.WriteLine(endMessage);
+            break;
+        }
+
+        await client.SendAsync(currentPacketInBytes, "localhost", 8080);
+        //Thread.Sleep(1);
+    }
+    
+    Thread.Sleep(1000);
+
+    //System.Console.WriteLine(imageInBytes.Length / packetLimit);
+    //await client.SendAsync(imageInBytes, "localhost", 8080);
+}
+
+
+
+// send text message
+if(false) {
+    var message = "Hello World!";
+
+    var messageInBytes = Encoding.Unicode.GetBytes(message);
+
+    await client.SendAsync(messageInBytes, "localhost", 8080);
+}
+
+//var endpoint = new IPEndPoint(IPAddress.Parse("172.10.10.5"), 8080);
+//await client.SendAsync(messageInBytes, messageInBytes.Length, endpoint);
