8b2f830e37833b9cf81a38ea51298fd597506754
Elnur Mustafayev <elnur.mustafayeev@gmail.com>
2023-11-21 21:31:38 +0400

    Add FTP and SMTP

---
+++ Network Programming/FTP/FtpClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using FluentFTP;
+
+FtpClient ftpClient = new FtpClient(host: "dewy.fem.tu-ilmenau.de");
+
+ftpClient.Connect();
+
+var files = ftpClient.GetListing(path: "/cdn.media.ccc.de/congress/2017/mp3/");
+
+// foreach (var item in files)
+// {
+//     System.Console.WriteLine(item.FullName);
+// }
+
+var remotePath = files[Random.Shared.Next(0, files.Length)].FullName;
+var remoteExtension = Path.GetExtension(remotePath);
+
+System.Console.WriteLine($"Loading {remotePath}...");
+ftpClient.DownloadFile(
+    localPath: $"rand{remoteExtension}", 
+    remotePath: remotePath);
+
+
+
+// ftpClient.DownloadFile(
+//     localPath: "bob.mp3", 
+//     remotePath: "/cdn.media.ccc.de/congress/2017/mp3/34c3-9104-eng-How_Alice_and_Bob_meet_if_they_dont_like_onions.mp3");
---
+++ Network Programming/SMTP/SmtpApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+var username = Environment.GetEnvironmentVariable("SMTP_USERNAME");
+var password = Environment.GetEnvironmentVariable("SMTP_PASSWORD");
+
+var client = new SmtpClient("sandbox.smtp.mailtrap.io", 2525) {
+    Credentials = new NetworkCredential(username, password),
+    EnableSsl = true
+};
+
+const string receiver = "jafar@example.com";
+
+MailMessage mailMessage = new MailMessage(
+    from: new MailAddress("elnur@example.com"),
+    to: new MailAddress(receiver)
+) {
+    IsBodyHtml = true,
+    Body = $@"
+    <div>
+        <h1 style=""text-align: center;"">Hello, {receiver}!</h1>
+
+        <button style=""background-color: green; color: white;"">Click this button!</button>
+    </div>",
+};
+
+client.Send(mailMessage);
