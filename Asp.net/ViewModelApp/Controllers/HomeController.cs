5c609cb9825d36e811d473cf7964deaa1a8aa607
Elnur Mustafayev <elnur.mustafayeev@gmail.com>
2024-04-16 21:56:42 +0400

    Add ViewBag, ViewData

---
+++ Asp.net/ViewModelApp/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ViewModelApp.Controllers;
+
+public class HomeController : Controller
+{
+    public async Task<IActionResult> Index()
+    {
+        // base.ViewBag.Username = "Ann";
+        // base.ViewData["name"] = "John";
+
+        return base.View();
+    }
+}
+
+
+
+// // GET: /User/Info
+// // Storage: /Asserts/user.json
+
+// {
+//     "name": "Bob",
+//     "surname": "Marley",
+// }
+
+// <label> <input:text readonly> </label>
+// Name: BOB
+// Surname: MARLEY
