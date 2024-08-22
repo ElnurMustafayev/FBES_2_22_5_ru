42351c926ec37aed4fd283a7335f0e3897772c10
Elnur Mustafayev <elnur.mustafayeev@gmail.com>
2024-08-22 21:22:58 +0400

    Add Docker network bridge

---
+++ Docker/dotnet/WebApiApp/test.ps1
@@ -0,0 +1,11 @@
+docker build -f postgres.Dockerfile -t mypostgres .
+docker run -d --rm -p 5432:5432 --name mypostgresdb --network=mynetwork -v mypostgresdb_data mypostgres
+
+docker build -f app.Dockerfile -t mywebapi .
+docker run -d --rm -p 5000:8080 --name mywebapiapp --network=mynetwork mywebapi
+
+docker exec -it mypostgresdb bash
+docker exec -it mywebapiapp bash
+
+apt update
+apt install -y dnsutils
