0938f59c4032125c4e2553528e78edaf9d95df31
Elnur Mustafayev <elnur.mustafayeev@gmail.com>
2024-08-24 15:16:46 +0400

    Add Docker-compose

--- Docker/dotnet/WebApiApp/test.ps1
+++ Docker/dotnet/WebApiApp/test.ps1
@@ -1,5 +1,5 @@
 docker build -f postgres.Dockerfile -t mypostgres .
-docker run -d --rm -p 5432:5432 --name mypostgresdb --network=mynetwork -v mypostgresdb_data mypostgres
+docker run -d --rm -p 5432:5432 --name mypostgresdb --network=mynetwork -v mypostgresdb_data:/var/lib/postgresql/data mypostgres
 
 docker build -f app.Dockerfile -t mywebapi .
 docker run -d --rm -p 5000:8080 --name mywebapiapp --network=mynetwork mywebapi
